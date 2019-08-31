fb5cf11eb48632764fb2168306c121dca8051c29
Nathan Genetzky <nathan@genetzky.us>
2019-08-30 22:45:03 -0500

    script: Modify to build all formats

--- script/_hackmyresume_build_all.sh
+++ script/_hackmyresume_build_all.sh
@@ -3,8 +3,10 @@
 
 resumePrefix=${1:-resume}
 
-resumeFile=$(ls /resumes/*resume.json | head -n 1)
+resumeFile='/resumes/resume.json'
 for theme in $(ls -d  /usr/lib/node_modules/jsonresume-theme*); do
     themeName=$(basename ${theme} | cut -d "-" -f3-)
-    hackmyresume build ${resumeFile} /resumes/${resumePrefix}-${themeName}.pdf -t ${theme}    
+    hackmyresume build ${resumeFile} \
+        "/resumes/${resumePrefix}-${themeName}.all" \
+        -t ${theme}    
 done
