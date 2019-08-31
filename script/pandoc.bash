5d9a5e2881c300d56f09543ebfadd07ad3c20274
Nathan Genetzky <nathan@genetzky.us>
2019-08-30 23:49:42 -0500

    script: Pandoc in a container

---
+++ script/pandoc.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+D_SCRIPT="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)"
+D_PROJ="${D_SCRIPT}/../"
+# IMAGE="hackmyresume"
+IMAGE="jagregory/pandoc"
+
+hackmyresume(){
+    cd "${D_PROJ}"
+    docker run -it --rm  \
+        -v  "$(pwd):/source/:ro" \
+        -v  "$(pwd)/build/:/work/" \
+        "${IMAGE}" \
+        "$@"
+
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    # Bash Strict Mode
+    set -eu -o pipefail
+
+    # set -x
+    hackmyresume "$@"
+fi
+
